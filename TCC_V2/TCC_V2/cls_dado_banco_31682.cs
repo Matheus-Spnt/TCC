28c8031744fc595b0c982cb3dd4a0a74fbef5bfe
Matheus-Spnt <88409141+Matheus-Spnt@users.noreply.github.com>
2023-09-18 13:11:57 -0300

    Páginas de resultado parcialmente prontas
    Páginas de resultado parcialmente prontas com o banco de dados

---
+++ TCC_V2/TCC_V2/cls_dado_banco_31682.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace cls_dado_banco_31682
+{
+    public class cls_dado_banco_31682
+    {
+        #region Strings
+        private string _linhaConexao;
+        MySqlConnection conexao = null;
+
+        public string linhaConexao
+        {
+            set { _linhaConexao = value; }
+            //get { return _linhaConexao; }
+        }
+        #endregion
+
+        #region Construtor
+        public cls_dado_banco_31682()
+        {
+
+        }
+        #endregion
+
+        #region Conectar
+        private bool Conectar()
+        {
+            bool worked = true;
+            conexao = new MySqlConnection(_linhaConexao);
+            try
+            {
+                conexao.Open();
+            }
+            catch
+            {
+                worked = false;
+            }
+            return worked;
+        }
+        #endregion
+
+        #region Consult
+        /// <summary>
+        /// Recupera dados da consulta do banco de dados
+        /// </summary>
+        public bool Consult(string comandoSELECT, ref MySqlDataReader dados)
+        {
+            if (Conectar())
+            {
+                bool worked = true;
+                MySqlCommand cSQL = new MySqlCommand(comandoSELECT, conexao);
+                try
+                {
+                    dados = cSQL.ExecuteReader();
+                }
+                catch
+                {
+                    worked = false;
+                }
+                return worked;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Executar
+        public bool Executar(string comando)
+        {
+            if (Conectar())
+            {
+                bool worked = true;
+                MySqlCommand cSQL = new MySqlCommand(comando, conexao);
+                try
+                {
+                    cSQL.ExecuteNonQuery();
+                }
+                catch
+                {
+                    worked = false;
+                }
+                Closing();
+                return worked;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Execute with paramiters
+        public bool Execute(string sql, List<MySqlParameter> valores)
+        {
+            try
+            {
+                bool worked = true;
+                if (Conectar())
+                {
+                    MySqlCommand cSQL = new MySqlCommand(sql, conexao);
+                    cSQL.Parameters.Clear();
+                    for (int i = 0; i < valores.Count; i++)
+                    {
+                        cSQL.Parameters.Add(valores[i]);
+                    }
+                    try
+                    {
+                        cSQL.ExecuteNonQuery();
+
+                    }
+                    catch
+                    {
+                        worked = false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                Closing();
+                return worked;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Close
+        public void Closing()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();                
+            }
+        }
+        #endregion
+    }
+}
---
+++ TCC_V2/TCC_V2/result_2.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+
+namespace TCC_V2
+{
+    public partial class result_2
+    {
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        protected global::System.Web.UI.WebControls.Label lbl_can2;
+        protected global::System.Web.UI.WebControls.Label lbl_can3;
+        protected global::System.Web.UI.WebControls.Label lbl_can4;
+    }
+
+    public partial class result_2 : System.Web.UI.Page
+    {
+        cls_dado_banco_31682.cls_dado_banco_31682 banco = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            banco = new cls_dado_banco_31682.cls_dado_banco_31682();
+            banco.linhaConexao = cls_con_banco_31682.cls_con_banco_31682.Local();
+            MySqlDataReader dados = null;
+
+
+            if (!banco.Consult("select c.nm_candidato, c.id_candidato, e.id_eleicao from candidato c join eleicao e on(c.id_eleicao = e.id_eleicao) where c.id_candidato = 1 and c.id_eleicao = 2;", ref dados))
+            {
+                Panel msg = new Panel();
+                Label lbl_can1_msg = new Label();
+                lbl_can1_msg.CssClass = "user_name_3";
+                lbl_can1_msg.Text = "Problemas na consulta ao servidor";
+                msg.Controls.Add(lbl_can1_msg);
+                Panel1.Controls.Add(msg);
+                banco.Closing();
+                return;
+            }
+
+            if (dados.HasRows)
+            {
+                while (dados.Read())
+                {
+                    Panel can1 = new Panel();
+                    Label lbl_can1 = new Label();
+                    lbl_can1.CssClass = "user_name_3";
+                    lbl_can1.Text = dados["nm_candidato"].ToString();
+                    can1.Controls.Add(lbl_can1);
+                    Panel1.Controls.Add(can1);
+                }
+            }
+
+
+            if (!banco.Consult("select c.nm_candidato, c.id_candidato, e.id_eleicao from candidato c join eleicao e on(c.id_eleicao = e.id_eleicao) where c.id_candidato = 2 and c.id_eleicao = 2;", ref dados))
+            {
+                lbl_can2.Text = "Problemas na consulta ao servidor";
+                banco.Closing();
+                return;
+            }
+
+            if (dados.HasRows)
+            {
+                while (dados.Read())
+                {
+
+                    lbl_can2.Text = dados["nm_candidato"].ToString();
+
+                }
+            }
+
+            if (!banco.Consult("select c.nm_candidato, c.id_candidato, e.id_eleicao from candidato c join eleicao e on(c.id_eleicao = e.id_eleicao) where c.id_candidato = 3 and c.id_eleicao = 2;", ref dados))
+            {
+                lbl_can3.Text = "Problemas na consulta ao servidor";
+                banco.Closing();
+                return;
+            }
+
+            if (dados.HasRows)
+            {
+                while (dados.Read())
+                {
+
+                    lbl_can3.Text = dados["nm_candidato"].ToString();
+
+                }
+            }
+
+            if (!banco.Consult("select c.nm_candidato, c.id_candidato, e.id_eleicao from candidato c join eleicao e on(c.id_eleicao = e.id_eleicao) where c.id_candidato = 4 and c.id_eleicao = 2;", ref dados))
+            {
+                lbl_can4.Text = "Problemas na consulta ao servidor";
+                banco.Closing();
+                return;
+            }
+
+            if (dados.HasRows)
+            {
+                while (dados.Read())
+                {
+
+                    lbl_can4.Text = dados["nm_candidato"].ToString();
+
+                }
+            }
+
+
+        }
+    }
+}
