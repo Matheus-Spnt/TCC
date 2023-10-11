ff3acdcddf4dd88649d802fddab6b32c5ebd7215
Matheus-Spnt <88409141+Matheus-Spnt@users.noreply.github.com>
2023-10-11 12:39:25 -0300

    Codigo 90%
    Codigo 90% falta teste

--- TCC_V2/TCC_V2/home_sc.aspx.cs
+++ TCC_V2/TCC_V2/home_sc.aspx.cs
@@ -19,13 +19,17 @@ namespace TCC_V2
 
     public partial class home_sc : System.Web.UI.Page
     {
-        int i;
-        String[] guarda = new String[i];
-        String[] guarda2 = new String[];
-        String[] guarda3 = new String[];
+        //int i;
+        //String[] guarda = new String[i];
+        //String[] guarda2 = new String[];
+        //String[] guarda3 = new String[];
+        string idEleicao;
+        string idEleicaoLocal;
+        string idEleicaoLocal_2;
         eleicao elect1 = new eleicao();
         usuario user1;
         cls_dado_banco_31682.cls_dado_banco_31682 banco = null;
+        
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -68,8 +72,7 @@ namespace TCC_V2
             {
                 while (dados.Read())
                 {
-                    guarda[i] = dados["e.id_eleicao"].ToString();
-
+                    idEleicao = dados["id_eleicao"].ToString();
                     Panel caixa1 = new Panel();
                     caixa1.CssClass = "box_resul_3";
                     Panel caixa2 = new Panel();
@@ -86,17 +89,33 @@ namespace TCC_V2
                     lbl_par1.Text = dados["descricao_eleicao"].ToString();
                     Panel caixa4 = new Panel();
                     caixa4.CssClass = "v_btn_4";
-                    Button v_btn = new Button();
-                    v_btn.CssClass = "v_btn_5";
+                    //Button v_btn = new Button();
+                    //v_btn.CssClass = "v_btn_5";
+                    //EventHandler p = (sender, e) => v_btn_Click(sender, e, idEleicao);
+                    //v_btn.Click += new EventHandler(p);
+
+                    foreach (Control control in Panel1.Controls)
+                    {
+                        if (control is Button)
+                        {
+                            Button votoButton = (Button)control;
+                            votoButton.CssClass = "v_btn_5";
+                            votoButton.Click += new EventHandler(v_btn_Click);
+                            votoButton.CommandArgument = idEleicao;
+                            caixa4.Controls.Add(votoButton);
+                        }
+                    }
+
+
                     caixa3.Controls.Add(img);
                     caixa3.Controls.Add(lbl_ti1);
                     caixa3.Controls.Add(lbl_par1);
-                    caixa4.Controls.Add(v_btn);
+                    //caixa4.Controls.Add(votoButton);
                     caixa2.Controls.Add(caixa3);
                     caixa2.Controls.Add(caixa4);
                     caixa1.Controls.Add(caixa2);
                     Panel1.Controls.Add(caixa1);
-                    i++;
+                    //i++;
                 }
             }
 
@@ -135,8 +154,8 @@ namespace TCC_V2
             {
                 while (dados.Read())
                 {
-                    guarda2[i] = dados["e.id_eleicao"].ToString();
-
+                    //guarda2[i] = dados["e.id_eleicao"].ToString();
+                    idEleicaoLocal = dados["id_eleicao"].ToString();
                     Panel caixa1 = new Panel();
                     caixa1.CssClass = "box_resul_3";
                     Panel caixa2 = new Panel();
@@ -156,18 +175,35 @@ namespace TCC_V2
                     v_texto.Text = "Em andamento";
                     Panel caixa4 = new Panel();
                     caixa4.CssClass = "v_btn_4";
-                    Button v_btn_2 = new Button();
-                    v_btn_2.CssClass = "v_btn_5";
+                    //Button v_btn_2 = new Button();
+                    //v_btn_2.CssClass = "v_btn_5";
+                    //v_btn_2.Attributes["data-id-eleicao"] = idEleicaoLocal;
+                    //EventHandler p = (sender, e) => v_btn_2Click(sender, e, idEleicao);
+                    //v_btn_2.Click += new EventHandler(p);
+
+                    foreach (Control control in Panel2.Controls)
+                    {
+                        if (control is Button)
+                        {
+                            Button andamentoButton = (Button)control;
+                            andamentoButton.Click += new EventHandler(v_btn_2Click);
+                            andamentoButton.CommandArgument = idEleicaoLocal;
+                            andamentoButton.CssClass = "v_btn_5";
+                            caixa4.Controls.Add(andamentoButton);
+                        }
+                    }
+
+
                     caixa3.Controls.Add(img);
                     caixa3.Controls.Add(lbl_ti2);
                     caixa3.Controls.Add(lbl_par2);
-                    caixa4.Controls.Add(v_btn_2);
+                    //caixa4.Controls.Add(v_btn_2);
                     caixa2.Controls.Add(caixa3);
                     caixa2.Controls.Add(v_texto);
                     caixa2.Controls.Add(caixa4);
                     caixa1.Controls.Add(caixa2);
                     Panel2.Controls.Add(caixa1);
-                    i++;
+                    //i++;
                 }
             }
             #endregion
@@ -205,8 +241,8 @@ namespace TCC_V2
             {
                 while (dados.Read())
                 {
-                    guarda3[i] = dados["e.id_eleicao"].ToString();
-
+                    //guarda3[i] = dados["e.id_eleicao"].ToString();
+                    idEleicaoLocal_2 = dados["id_eleicao"].ToString();
                     Panel caixa1 = new Panel();
                     caixa1.CssClass = "box_resul_3";
                     Panel caixa2 = new Panel();
@@ -226,39 +262,80 @@ namespace TCC_V2
                     v_texto.Text = "Em andamento";
                     Panel caixa4 = new Panel();
                     caixa4.CssClass = "v_btn_4";
-                    Button v_btn_3 = new Button();
-                    v_btn_3.CssClass = "v_btn_5";
+                    //Button v_btn_3 = new Button();
+                    //v_btn_3.CssClass = "v_btn_5";
+
+                    foreach (Control control in Panel3.Controls)
+                    {
+                        if (control is Button)
+                        {
+                            Button finalizadoButton = (Button)control;
+                            finalizadoButton.Click += new EventHandler(v_btn_3Click);
+                            finalizadoButton.CommandArgument = idEleicaoLocal_2;
+                            finalizadoButton.CssClass = "v_btn_5";
+                            caixa4.Controls.Add(finalizadoButton);
+                        }
+                    }
+
+
                     caixa3.Controls.Add(img);
                     caixa3.Controls.Add(lbl_ti3);
                     caixa3.Controls.Add(lbl_par3);
-                    caixa4.Controls.Add(v_btn_3);
+                    //caixa4.Controls.Add(v_btn_3);
                     caixa2.Controls.Add(caixa3);
                     caixa2.Controls.Add(v_texto);
                     caixa2.Controls.Add(caixa4);
                     caixa1.Controls.Add(caixa2);
                     Panel2.Controls.Add(caixa1);
-                    i++;
+                    //i++;
                 }
             }
             #endregion
         }
 
-        private void v_btn(object sender, EventArgs e)
-        {
-            elect1.SetEleicaoId(guarda[i]);
-            Response.Redirect("~/voto_sc.aspx");
+        //private void v_btn_Click(object sender, EventArgs e, string idEleicao)
+        //{
+        //    elect1.SetEleicaoId(idEleicao);
+        //    Response.Redirect("~/voto_sc.aspx");
+        //}
 
+        protected void v_btn_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string idEleicao = clickedButton.CommandArgument;
+            elect1.SetEleicaoId(idEleicao);
+            Response.Redirect("~/voto_sc.aspx");
         }
 
-        private void v_btn_2(object sender, EventArgs e)
+
+        //private void v_btn_2Click(object sender, EventArgs e, string idEleicao)
+        //{
+        //    Button clickedButton = (Button)sender;
+        //    idEleicao = clickedButton.Attributes["data-id-eleicao"];
+        //    elect1.SetEleicaoId(idEleicao);
+        //    Response.Redirect("~/result.aspx");
+        //}
+
+        protected void v_btn_2Click(object sender, EventArgs e)
         {
-            elect1.SetEleicaoId(guarda2[i]);
+            Button clickedButton = (Button)sender;
+            string idEleicao = clickedButton.CommandArgument;
+            elect1.SetEleicaoId(idEleicao);
             Response.Redirect("~/result.aspx");
         }
 
-        private void v_btn_3(object sender, EventArgs e)
+
+        //private void v_btn_3Click(object sender, EventArgs e, string idEleicao)
+        //{
+        //    elect1.SetEleicaoId(idEleicao);
+        //    Response.Redirect("~/result_2.aspx");
+        //}
+
+        protected void v_btn_3Click(object sender, EventArgs e)
         {
-            elect1.SetEleicaoId(guarda3[i]);
+            Button clickedButton = (Button)sender;
+            string idEleicao = clickedButton.CommandArgument;
+            elect1.SetEleicaoId(idEleicao);
             Response.Redirect("~/result_2.aspx");
         }
 
