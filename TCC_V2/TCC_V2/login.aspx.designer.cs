e322134fede0cc454e27d40426711b242ba29579
Matheus-Spnt <88409141+Matheus-Spnt@users.noreply.github.com>
2023-10-23 21:07:22 -0300

    Referencias Corrigidas
    Referencias Corrigidas em sua maior parte

--- TCC_V2/TCC_V2/login.aspx.designer.cs
+++ TCC_V2/TCC_V2/login.aspx.designer.cs
@@ -11,106 +11,10 @@ namespace TCC_V2
 {
 
 
-    public partial class cads
+    public partial class login
     {
-
-        /// <summary>
-        /// Controle cad_user.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user;
-
-        /// <summary>
-        /// Controle cad_user_nasc.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_nasc;
-
-        /// <summary>
-        /// Controle cad_user_ender.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_ender;
-
-        /// <summary>
-        /// Controle cad_user_titulo.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_titulo;
-
-        /// <summary>
-        /// Controle cad_user_zona.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_zona;
-
-        /// <summary>
-        /// Controle cad_user_sec.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_sec;
-
-        /// <summary>
-        /// Controle cad_user_pass1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_pass1;
-
-        /// <summary>
-        /// Controle cad_user_pass2.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_pass2;
-
-        /// <summary>
-        /// Controle cad_user_cpf.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox cad_user_cpf;
-
-        /// <summary>
-        /// Controle btn_cad.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn_cad;
-
-        /// <summary>
-        /// Controle lblMsg.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox log_user;
+        protected global::System.Web.UI.WebControls.TextBox log_user_pass1;
         protected global::System.Web.UI.WebControls.Label lblMsg;
     }
 }
